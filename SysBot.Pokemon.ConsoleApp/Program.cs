8ed1a7e71e01ec61c0bc8cdc5f1197d44ef6851a
Easy World <easy_world_118@Hotmail.com>
2023-09-05 11:20:49 +0800

    Support new API

--- SysBot.Pokemon.ConsoleApp/Program.cs
+++ SysBot.Pokemon.ConsoleApp/Program.cs
@@ -1,12 +1,9 @@
 ﻿using PKHeX.Core;
 using SysBot.Base;
-using SysBot.Pokemon.Z3;
 using System;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using PKHeX.Core;
-using SysBot.Base;
 
 namespace SysBot.Pokemon.ConsoleApp
 {
